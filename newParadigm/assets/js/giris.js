ac37cdbf785a8f8ddaece2b60557f0112a0ba95e
SuleymanCanTugcu <96788480+SuleymanCanTugcu@users.noreply.github.com>
2022-11-12 18:33:08 +0300

    18.32

---
+++ newParadigm/assets/js/giris.js
@@ -0,0 +1,35 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var s = document.getElementById('jobs');
+    var jobs = s.options[s.selectedIndex].value;
+    if (username == "Formget" && password == "formget#123" && jobs == "doktor") {
+        alert("Login successfully");
+        window.location = "doktor.html"
+        return false;
+    }
+    else if((username == "Formget" && password == "formget#125" && jobs == "Dagıtımcı")){
+        alert("Login successfully");
+        window.location = "Dagıtımcı.html"
+        return false;
+    }
+    else {
+        attempt--;// Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+
+
+
