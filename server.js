d27ed1b26b0bc2097f526737860462a09fb903e7
Halim-m <70447318+Halim-m@users.noreply.github.com>
2022-11-12 15:05:39 +0300

    newParadigm1.2
    UI integration

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ import exprss from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-import { getAll, writePills, getData } from "./assets/firebaseHelper.js";
+import { getAll, writePills, getData } from "./assets/js/firebaseHelper.js";
 
 const app = exprss();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
+
 //how to using res.sendFile ?
-app.use("/js", exprss.static('./assets/'));
+
+app.use('/assets', exprss.static(__dirname + '/assets'));
+app.use(exprss.static(__dirname + '/public'));
+
+
+app.use("/js", exprss.static('./assets/js/'));
+app.use("/js1", exprss.static('./assets/js/main.js'));
+app.use("/js2", exprss.static('./assets/js/giris.js'));
+app.use("/css", exprss.static('./assets/css/'));
+app.use("/img", exprss.static('./assets/img/'));
 app.get('/', (_req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 
